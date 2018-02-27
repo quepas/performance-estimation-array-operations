d44be6eb763f54f2b1a030f02b5553eadac2efcc
quepas <kiepas@agh.edu.pl>
2018-02-28 00:32:12 +0100

    Changing paths/env to estimate

--- estimation/runAnalysis.R
+++ estimation/runAnalysis.R
@@ -5,18 +5,18 @@ source("estimateNNLogistic.R")
 
 ### CHANGE THIS !!! ###
 # >> Where your measurements are?
-measurementsDir <- "../backup/RUN5_23_02_32MB_Py2Py3/"
+measurementsDir <- "../paper-experiments/results/measurements"
 # >> What performance aspects you are analyzing? (TIME + PAPI events)
-aspects <- c("TIME", "PAPI_L1_DCM", "PAPI_TOT_INS") #PAPI_BR_INS", "PAPI_LST_INS", "PAPI_TOT_INS")
+aspects <- c("TIME", "PAPI_L1_DCM", "PAPI_TOT_INS")
 # >> For what languages/environments you did run your analysis? (names of directories inside measurementsDir)
-languages <- c("Python2", "Python3")#R14b_ST", "R14b_MT", "R16b_ST", "R16b_MT", "Octave", "Scilab")
+languages <- c("Python2", "Python3", "R14b_ST", "R14b_MT", "R16b_ST", "R16b_MT", "Octave", "Scilab")
 #######################
 
 result <- list()
 # For each performance aspect
 for (aspect in aspects) {
   cat(sprintf("%s\n", aspect))
-  
+
   aspectResults <- data.frame(nums = 1:8) # 8 == num. of estimators * 2 (two methods: naive and one-sided)
   # For each language
   for (lang in languages) {
